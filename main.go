393d7b35a378f7dede7f2e06d84271958bd8d648
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2024-10-25 22:03:04 +0200

    added multithreading

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
+	"time"
 )
 
 func main() {
@@ -18,10 +20,33 @@ func main() {
 	}
 
 	fmt.Printf("\n ==starting crawl of: %s ==\n", args[0])
-	pages := make(map[string]int, 0)
-	crawlPage(args[0], args[0], pages)
-	for k, v := range pages {
+	url, err := url.Parse(args[0])
+	if err != nil {
+		fmt.Printf("could not parse URL: %v", err)
+		os.Exit(1)
+	}
+
+	cfg := NewConfig(10, url)
+	crawl(cfg, *url)
+	for k, v := range cfg.pages {
 		fmt.Printf("site: %s count: %d \n", k, v)
 	}
 	fmt.Print("\n ==Finished== \n")
 }
+
+func crawl(cfg *config, url url.URL) {
+	defer trackTime(time.Now(), "Crawling")
+	cfg.wg.Add(1)
+	go cfg.crawlPage(url.String())
+	cfg.wg.Wait()
+}
+
+func trackTime(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s\n", name, elapsed)
+}
+
+//1 Thread Crawling took 7.562786s
+//2 Threads Crawling took 3.596488834s
+//5 Threads Crawling took 1.825310417s
+//10 Threads Crawling took 1.014717666s
--- normalize_url.go
+++ normalize_url.go
@@ -89,6 +89,45 @@ func getHTML(rawURL string) (string, error) {
 	return string(body), nil
 }
 
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	defer cfg.wg.Done()
+	defer func() { <-cfg.concurrencyControl }()
+	cfg.concurrencyControl <- struct{}{}
+
+	log.Printf("Crawling: %v\n", rawCurrentURL)
+	rawCurrentURLNormalized, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error normalizing URL from %s, err: %v\n", rawCurrentURL, err)
+		return
+	}
+
+	if !strings.Contains(rawCurrentURL, cfg.baseURL.String()) {
+		fmt.Printf("Aborting, would leave %v to go to %v\n", cfg.baseURL.String(), rawCurrentURL)
+		return
+	}
+
+	isFirst := cfg.addPageVisit(rawCurrentURLNormalized)
+	if !isFirst {
+		return
+	}
+
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error getting HTML from %s, err: %v\n", rawCurrentURL, err)
+		return
+	}
+
+	urls, err := getURLsFromHTML(html, rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error getting URLs from %s, err: %v\n", rawCurrentURL, err)
+		return
+	}
+	for _, v := range urls {
+		cfg.wg.Add(1)
+		go cfg.crawlPage(v)
+	}
+}
+
 func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 	log.Printf("Crawling: %v\n", rawCurrentURL)
 	rawCurrentURLNormalized, err := normalizeURL(rawCurrentURL)
@@ -107,6 +146,7 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 		return
 	}
 	pages[rawCurrentURLNormalized] = 1
+
 	html, err := getHTML(rawCurrentURL)
 	if err != nil {
 		fmt.Printf("Error getting HTML from %s, err: %v\n", rawCurrentURL, err)
@@ -120,5 +160,26 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 	}
 	for _, v := range urls {
 		crawlPage(rawCurrentURLNormalized, v, pages)
+	}
+}
+
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	if _, ok := cfg.pages[normalizedURL]; ok {
+		cfg.pages[normalizedURL]++
+		return false
+	}
+	cfg.pages[normalizedURL] = 1
+	return true
+}
+
+func NewConfig(maxConcurrency int, baseURL *url.URL) *config {
+	return &config{
+		pages:              map[string]int{},
+		baseURL:            baseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
 	}
 }
